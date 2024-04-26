94770ec14df5ec7b3db8af1f21f51fadfbe2a57f
Jaheda20 <jahedasultana20@gmail.com>
2024-04-26 09:56:44 +0200

    completed error page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,10 +1,17 @@
 import Lottie from "lottie-react";
 import errorAnimation from "../assets/404error.json"
+import { Link } from "react-router-dom";
+import { FaArrowCircleLeft } from "react-icons/fa";
+
 
 const ErrorPage = () => {
     return (
-        <div>
-           <Lottie animationData={errorAnimation} loop={true} />;
+        <div className="flex flex-col items-center justify-center">
+            <div className="w-96 pt-12">
+                <Lottie animationData={errorAnimation} loop={true} />
+            </div>
+            <Link to="/" className="pt-10 hover:underline hover:text-indigo-800 flex items-center"> <span className="mr-3"> <FaArrowCircleLeft /> </span>Back to Homepage</Link>
+
         </div>
     );
 };
